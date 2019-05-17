5d37f34cb52148185a1522260fb465f6eb552638
Patricio Toledo <patotomastoledo6@gmail.com>
2019-05-17 20:01:29 -0300

    El modelo edita correctamente

--- test/unit/model.test.js
+++ test/unit/model.test.js
@@ -69,3 +69,37 @@ test('Obtener película', async () => {
 
     // Completar test
 });
+
+test('Editar pelicula', async () =>{
+    const movieData = {
+        title: 'El dia que me quieras',
+        description: 'Carlos Argüelles is the son of a wealthy man whose only interests in life are business and making money. While trying to succeed in show business he falls in love with a dancer and they elope to marry. But success is not easy to obtain.',
+        year: 1935,
+        runtime: 82,
+        country: 'United States',
+        language: 'Spanish',
+        genres: ['Drama'],
+        directors: ['John Reinhardt'],
+        writers: ['Alfredo Le Pera']
+    };
+    const movieDataUpdate = {
+        title: 'Back to the Future',
+        description: 'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.',
+        year: 1985,
+        runtime: 116,
+        country: 'United States',
+        language: 'English',
+        genres: ['Adventure', 'Comedy', 'Science Fiction'],
+        directors: ['Robert Zemeckis'],
+        writers: ['Robert Zemeckis', 'Bob Gale']
+    };
+    //Insertamos pelicula
+    const movieCreated = await MovieModels.create(movieData);    
+    //Modifico con la otra pelicula
+    const updatedMovie = await MovieModels.update(movieCreated.id,movieDataUpdate);
+    //Voy a ver como quedo guardada
+    const recivedMovie = await MovieModels.get(updatedMovie.id);   
+    //Comparo lo que subi al update, con lo que modifico
+    expect(recivedMovie.title).toBe(movieDataUpdate.title);
+    expect(recivedMovie.year).toBe(movieDataUpdate.year);       
+});

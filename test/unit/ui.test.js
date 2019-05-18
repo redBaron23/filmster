9aa3271c34873de88d311cdee2855b63b5a167e1
Patricio Toledo <patotomastoledo6@gmail.com>
2019-05-17 22:48:30 -0300

    Le subo de nivel al test

--- test/unit/ui.test.js
+++ test/unit/ui.test.js
@@ -1,7 +1,7 @@
 const utils = require('../../client/src/utils.mjs');
 
-test.skip('parseCSV', () => {
-    const actual = utils.parseCSV('foo, bar')
+test('parseCSV', () => {
+    const actual = utils.parseCSV('  foo  , bar  ')
     const expected = ['foo', 'bar']
 
     expect(actual).toStrictEqual(expected)

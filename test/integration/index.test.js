ae0078bb488a7532a6bd76b3e00ab1f74351005f
Matias <cstocki128@gmail.com>
2019-05-19 18:34:32 -0300

    IntegrationTest de Api Pelicula y comprobar Bd

--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -19,3 +19,40 @@ test('Se debería iniciar la aplicación sin películas', async () => {
 
     expect(movies.length).toBe(0)
 });
+
+test('Se deberia poder agregar una pelicula usando la Api y verificar que exista la pelicula en bd', async () => { 
+	//Se declara la pelicula
+	const movie = {
+        title: 'Back to the Future',
+        description: 'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a time-traveling DeLorean invented by his close friend, the maverick scientist Doc Brown.',
+        year: 1985,
+        runtime: 116,
+        country: 'United States',
+        language: 'English',
+        genres: ['Adventure', 'Comedy', 'Science Fiction'],
+        directors: ['Robert Zemeckis'],
+        writers: ['Robert Zemeckis', 'Bob Gale']
+    	};
+
+	//Se llama a la API
+	await fetch(`${baseURL}/movies`, {
+        method: 'POST', 
+        body: JSON.stringify(movie),
+        headers:{
+            'Content-Type': 'application/json'
+        }});
+
+	//Se Obtiene la pelicula
+    	const req = await fetch(`${baseURL}/movies/1`)
+    	const receivedMovie = await req.json()
+ 
+    	expect(movie.title).toBe(receivedMovie.title)
+    	expect(movie.description).toBe(receivedMovie.description)
+    	expect(movie.year).toBe(receivedMovie.year)
+    	expect(movie.runtime).toBe(receivedMovie.runtime)
+    	expect(movie.country).toBe(receivedMovie.country)
+    	expect(movie.language).toBe(receivedMovie.language)
+    	expect(movie.genres).toStrictEqual(receivedMovie.genres)
+    	expect(movie.directors).toStrictEqual(receivedMovie.directors)
+    	expect(movie.writers).toStrictEqual(receivedMovie.writers)
+});
